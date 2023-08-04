0fc8587ccc6a12296bdcdc58c7c2923c01e373fb
moon-jam <wu.messi.wu@gmail.com>
2023-08-04 17:10:18 +0800

    ranking data process

--- tool/vid_User.py
+++ tool/vid_User.py
@@ -1,9 +1,9 @@
 import json
 
 def write(file, gmail):
-    with open("vid_User.json", 'r') as f:
+    with open("data/vid_User.json", 'r') as f:
         content = json.load(f)
     input = {file:gmail}
     content.update(input)
-    with open("vid_User.json", 'w') as f_new:
+    with open("data/vid_User.json", 'w') as f_new:
         json.dump(content, f_new)
